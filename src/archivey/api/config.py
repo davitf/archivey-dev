e3c55aa24a8a12de102d09dad68d64651d153e9d
davitf <davitf>
2025-07-01 08:02:37 -0300

    Fix doc config field descriptions

--- src/archivey/api/config.py
+++ src/archivey/api/config.py
@@ -26,44 +26,32 @@ class OverwriteMode(StrEnum):
 class ArchiveyConfig:
     """Configuration for :func:`archivey.open_archive`."""
 
-    # These are alternative libraries that can be used instead of the builtin modules
-    # to read these stream formats. They provide multithreaded decompression and
-    # improved random access support (i.e. they support jumping to arbitrary positions
-    # in the stream without (re-)decompressing the entire stream, which is particularly
-    # useful for accessing random members in compressed tar files).
     use_rapidgzip: bool = False
+    "Alternative library that can be used instead of the builtin modules to read gzip stream formats. Provides multithreaded decompression and improved random access support (i.e. supports jumping to arbitrary positions in the stream without (re-)decompressing the entire stream, which is particularly useful for accessing random members in compressed tar files)."
+    
     use_indexed_bzip2: bool = False
+    "Alternative library that can be used instead of the builtin modules to read bzip2 stream formats. Provides multithreaded decompression and improved random access support."
+    
     use_python_xz: bool = False
+    "Alternative library that can be used instead of the builtin modules to read xz stream formats. Provides multithreaded decompression and improved random access support."
 
-    # An alternative to pyzstd. Not as good at error reporting.
     use_zstandard: bool = False
+    "An alternative to pyzstd. Not as good at error reporting."
 
-    # If set, use an alternative approach instead of calling rarfile when iterating
-    # over RAR archive members. This supports decompressing multiple members in a solid
-    # archive by going through the archive only once, instead of once per member.
     use_rar_stream: bool = False
+    "If set, use an alternative approach instead of calling rarfile when iterating over RAR archive members. This supports decompressing multiple members in a solid archive by going through the archive only once, instead of once per member."
 
-    # If set, data stored in compressed stream headers is set in the ArchiveMember
-    # object for single-file compressed archives, instead of basing it only on the
-    # file itself. (filename and modification time for gzip archives only)
     use_single_file_stored_metadata: bool = False
+    "If set, data stored in compressed stream headers is set in the ArchiveMember object for single-file compressed archives, instead of basing it only on the file itself. (filename and modification time for gzip archives only)"
 
-    # If a tar archive is corrupted in a metadata section, tarfile simply stops reading
-    # further and acts as if the file has ended. If set, we perform a check that the tar
-    # archive has actually been read fully, and raise an error if it's actually
-    # corrupted.
     tar_check_integrity: bool = True
+    "If a tar archive is corrupted in a metadata section, tarfile simply stops reading further and acts as if the file has ended. If set, we perform a check that the tar archive has actually been read fully, and raise an error if it's actually corrupted."
 
-    # What to do with existing files when extracting.
-    # - OVERWRITE: overwrite existing files
-    # - SKIP: skip existing files
-    # - ERROR: raise an error if a file already exists, and stop extracting
     overwrite_mode: OverwriteMode = OverwriteMode.ERROR
+    "What to do with existing files when extracting. OVERWRITE: overwrite existing files. SKIP: skip existing files. ERROR: raise an error if a file already exists, and stop extracting."
 
-    # A filter function that can be used to filter members when iterating over an
-    # archive. It can be a function that takes an ArchiveMember and returns a
-    # possibly-modified ArchiveMember object, or None to skip the member.
     extraction_filter: ExtractionFilter | FilterFunc = ExtractionFilter.DATA
+    "A filter function that can be used to filter members when iterating over an archive. It can be a function that takes an ArchiveMember and returns a possibly-modified ArchiveMember object, or None to skip the member."
 
 
 _default_config_var: contextvars.ContextVar[ArchiveyConfig] = contextvars.ContextVar(
